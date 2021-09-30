a7d78619bb2962af9ac367a29df1a335c0ea75c0
bradyrussell <31458670+bradyrussell@users.noreply.github.com>
2021-09-29 20:17:50 -0500

    Add project files.

---
+++ TSqlTools/TSqlDescriptionGenerator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TSqlTools
+{
+    class TSqlDescriptionGenerator
+    {
+        public static string GenerateDescriptions(string input)
+        {
+            return TSqlToolsLib.TSqlTools.GenerateDescriptions(input);
+        }
+    }
+}
