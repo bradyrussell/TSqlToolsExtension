ca93462c5188e1c34487f86ae32e3bea7e128c08
Brady Russell <31458670+bradyrussell@users.noreply.github.com>
2022-06-14 15:24:32 -0500

    annotate code values

---
+++ TSqlTools/AnnotateCodeValuesCommand.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using System;
+using System.ComponentModel.Design;
+using System.Globalization;
+using System.Threading;
+using System.Threading.Tasks;
+using EnvDTE;
+using Task = System.Threading.Tasks.Task;
+
+namespace TSqlTools
+{
+    /// <summary>
+    /// Command handler
+    /// </summary>
+    internal sealed class AnnotateCodeValuesCommand
+    {
+        /// <summary>
+        /// Command ID.
+        /// </summary>
+        public const int CommandId = 4130;
+
+        /// <summary>
+        /// Command menu group (command set GUID).
+        /// </summary>
+        public static readonly Guid CommandSet = new Guid("4e9654a5-1af8-4bbd-8c74-4ed53f3375e2");
+
+        /// <summary>
+        /// VS Package that provides this command, not null.
+        /// </summary>
+        private readonly AsyncPackage package;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AnnotateCodeValuesCommand"/> class.
+        /// Adds our command handlers for menu (commands must exist in the command table file)
+        /// </summary>
+        /// <param name="package">Owner package, not null.</param>
+        /// <param name="commandService">Command service to add command to, not null.</param>
+        private AnnotateCodeValuesCommand(AsyncPackage package, OleMenuCommandService commandService)
+        {
+            this.package = package ?? throw new ArgumentNullException(nameof(package));
+            commandService = commandService ?? throw new ArgumentNullException(nameof(commandService));
+
+            var menuCommandID = new CommandID(CommandSet, CommandId);
+            var menuItem = new MenuCommand(this.Execute, menuCommandID);
+            commandService.AddCommand(menuItem);
+        }
+
+        /// <summary>
+        /// Gets the instance of the command.
+        /// </summary>
+        public static AnnotateCodeValuesCommand Instance
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// Gets the service provider from the owner package.
+        /// </summary>
+        private Microsoft.VisualStudio.Shell.IAsyncServiceProvider ServiceProvider
+        {
+            get
+            {
+                return this.package;
+            }
+        }
+
+        /// <summary>
+        /// Initializes the singleton instance of the command.
+        /// </summary>
+        /// <param name="package">Owner package, not null.</param>
+        public static async Task InitializeAsync(AsyncPackage package)
+        {
+            // Switch to the main thread - the call to AddCommand in AnnotateCodeValuesCommand's constructor requires
+            // the UI thread.
+            await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync(package.DisposalToken);
+
+            OleMenuCommandService commandService = await package.GetServiceAsync(typeof(IMenuCommandService)) as OleMenuCommandService;
+            Instance = new AnnotateCodeValuesCommand(package, commandService);
+        }
+
+        /// <summary>
+        /// This function is the callback used to execute the command when the menu item is clicked.
+        /// See the constructor to see how the menu item is associated with this function using
+        /// OleMenuCommandService service and MenuCommand class.
+        /// </summary>
+        /// <param name="sender">Event sender.</param>
+        /// <param name="e">Event args.</param>
+        private void Execute(object sender, EventArgs e)
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+            DTE dte = Package.GetGlobalService(typeof(DTE)) as DTE;
+            Document doc = dte.ActiveDocument;
+            TextDocument textDocument = doc.Object() as TextDocument;
+            var edit = textDocument.CreateEditPoint();
+            var text = edit.GetText(textDocument.EndPoint);
+            edit.Delete(textDocument.EndPoint);
+            edit.Insert(TSqlDescriptionGenerator.GenerateCodeValueAnnotations(text));
+        }
+    }
+}
--- TSqlTools/TSqlDescriptionGenerator.cs
+++ TSqlTools/TSqlDescriptionGenerator.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.SqlServer.Management.Smo.RegSvrEnum;
+using Microsoft.SqlServer.Management.UI.VSIntegration;
+using Microsoft.SqlServer.Management.UI.VSIntegration.Editors;
 
 namespace TSqlTools
 {
@@ -16,6 +20,15 @@ namespace TSqlTools
         public static string GenerateComments(string input)
         {
             return TSqlToolsLib.TSqlTools.GenerateCommentsFromDescriptions(input);
+        }
+
+        public static string GenerateCodeValueAnnotations(string input)
+        {
+            IScriptFactory scriptFactory = ServiceCache.ScriptFactory;
+            CurrentlyActiveWndConnectionInfo connectionIfno = scriptFactory.CurrentlyActiveWndConnectionInfo;
+            UIConnectionInfo conn = connectionIfno.UIConnectionInfo;
+            Debug.WriteLine($"Server: {conn.ServerName}, Database: {conn.AdvancedOptions["DATABASE"]}");
+            return TSqlToolsLib.TSqlTools.AnnotateCodeValues(conn.ServerName, conn.AdvancedOptions["DATABASE"], input);
         }
     }
 }
